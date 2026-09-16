0fdfb608276d04651fd5b38f4e7a7047d59d5b65
angga118 <anggaiqbal86@gmail.com>
2026-09-16 13:01:49 +0700

    Switch to PrismaPg adapter for Prisma Postgres (db.prisma.io)

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,8 @@
 import { PrismaClient } from "../src/generated/prisma/client";
-import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaPg } from "@prisma/adapter-pg";
 import bcrypt from "bcryptjs";
 
-const adapter = new PrismaNeon({
+const adapter = new PrismaPg({
   connectionString:
     process.env.POSTGRES_URL ?? process.env.DATABASE_URL ?? "",
 });
